10a8967870af8616eef39f6a6292ac00231b77df
ahmeetguns <ahmeetguns@gmail.com>
2024-11-18 15:10:00 +0100

    2D Animation

---
+++ src/MyFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+    MyPanel panel;
+    MyFrame(){
+        panel = new MyPanel();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.add(panel);
+        this.pack();
+        this.setVisible(true);
+    }
+}
